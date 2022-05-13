1e8b2d2050088bb3bb90fc16662ab9a3832c0800
NATALIA HILARION <77810734+NataliaHilarion@users.noreply.github.com>
2022-05-12 19:42:58 -0500

    Add files via upload

--- javascript/display.js
+++ javascript/display.js
@@ -8,7 +8,7 @@ export default class Display {
     this.currentNumber = '';
     this.previousNumber = '';
     this.operator = undefined;
-    this.signos = {
+    this.signs = {
       add: '+',
       divide: '/',
       subtract: '-',
@@ -16,24 +16,55 @@ export default class Display {
     }
   }
 
+  // agrega el numero que se activo con el evento click y lo muestra en el display
   addNumber(number) {
     if (number === '.' && this.currentNumber.includes('.')) return
     this.currentNumber = this.currentNumber.toString() + number.toString()
     this.printValues();
   }
 
+  // borra el ultimo caracter del numero actual, si el string esta vacio, imprime el numero 0
   deleteNumber() {
     this.currentNumber = this.currentNumber.toString().slice(0, -1)
     this.printValues()
+    if (this.currentNumber == '' && this.previousNumber == '') {
+      this.initialitation()
+    }
   }
 
+  //borra todo el contenido del display, e imprime el numero 0
   deleteAll() {
     this.currentNumber = ''
     this.previousNumber = ''
     this.operator = undefined
     this.printValues()
+    this.initialitation()
   }
 
+  // es el metodo que se usa en los otros metodos delete, hace que en el display aparezca el 0
+  initialitation() {
+    this.$displayCurrentValue.textContent = 0
+  }
+
+  percentage() {
+    const percent = this.previousNumber / 100
+    this.$displayPreviousValue.textContent = percent
+    this.currentNumber = ''
+    this.$displayCurrentValue.textContent = this.currentNumber
+
+  }
+
+  negativeNumber() {
+    if (this.previousNumber !== '') {
+      this.previousNumber = this.previousNumber * -1
+      this.$displayPreviousValue.textContent = this.previousNumber
+      this.$displayCurrentValue.textContent = ''
+    }
+    this.currentNumber = this.currentNumber * -1
+    this.$displayCurrentValue.textContent = this.currentNumber
+  }
+
+  // al detectar un valor diferente del equal, llama a la funcion calculate
   doTheOperation(value) {
     if (this.operator !== 'equal') {
       this.calculate();
@@ -44,11 +75,14 @@ export default class Display {
     this.printValues()
   }
 
+  // metodo reutilizable en otros metodos, se encarga de imprimir valores en el display
   printValues() {
     this.$displayCurrentValue.textContent = this.currentNumber
-    this.$displayPreviousValue.textContent = `${this.previousNumber} ${this.signos[this.operator] || ''}   `
+    this.$displayPreviousValue.textContent = `${this.previousNumber} ${this.signs[this.operator] || ''}   `
+
   }
 
+  //calculos que se realizan con la clase Calculator que esta en el modulo Calculator.js
   calculate() {
     const previousNumber = parseFloat(this.previousNumber)
     const currentNumber = parseFloat(this.currentNumber)
@@ -56,6 +90,8 @@ export default class Display {
     if (isNaN(currentNumber) || isNaN(previousNumber)) return
     this.currentNumber = this.calculator[this.operator](previousNumber, currentNumber)
   }
+
+
 
 }
 
--- javascript/index.js
+++ javascript/index.js
@@ -7,17 +7,30 @@ const $numberButton = document.querySelectorAll('.number')
 const $operator = document.querySelectorAll('.operator')
 const $back = document.querySelector('.back')
 const $delete = document.querySelector('.delete')
+const $percentage = document.getElementById('percentage')
+const $negative = document.getElementById('negative')
 
 const display = new Display($displayPreviousValue, $displayCurrentValue)
 
+// Iteramos los elementos con la clase number y su texto de HTML se pasa como argumento al metodo addNumber de la clase Display
 $numberButton.forEach(button => {
   button.addEventListener('click', () => display.addNumber(button.innerHTML) )
 })
 
+// Cada vez que demos click en boton con la clase back se activa el metodo deleteNumber de la clase Display
 $back.addEventListener('click', () =>  display.deleteNumber() )
 
+// Cada vez que demos click en boton con la clase delete se activa el metodo deleteAll de la clase Display
 $delete.addEventListener('click', () => display.deleteAll())
 
+// Iteramos los elementos con la clase operator y su value se pasa como argumento al metodo doTheOperation de la clase Display
 $operator.forEach( button => {
   button.addEventListener('click', () => display.doTheOperation(button.value) )
 })
+
+// el evento click del id percentage llama al metodo percentage de la clase display
+$percentage.addEventListener('click', () => display.percentage())
+
+// el evento click del id negative llama al metodo negativeNumber de la clase display
+$negative.addEventListener('click', () => display.negativeNumber())
+
